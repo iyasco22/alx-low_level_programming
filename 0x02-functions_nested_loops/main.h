5657e3c7f8d195b27338efe007808036d25797ee
Iyanuoluwa Olorundare <you@example.com>
2023-09-12 15:21:21 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,29 @@
 int _putchar(char c);
 void print_alphabet(void);
 
+int _putchar(char c);
+
+int _putchar(char c);
+void print_alphabet_x10(void);
+
+int _putchar(char c);
+int _islower(int c);
+
+int _putchar(char c);
+int _isalpha(int c);
+
+int _putchar(char c);
+int print_sign(int n);
+
+int _putchar(char c);
+int _abs(int n);
+
+int _putchar(char c);
+int print_last_digit(int n);
+
+int _putchar(char c);
+void jack_bauer(void);
+
+int _putchar(char c);
+void times_table(void);
 #endif /* MAIN_H */
